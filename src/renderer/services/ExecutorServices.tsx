7299da11e5ea853883b884225ce1d1dc18f8e57c
Lim Junxue <junxue1@hotmail.com>
2023-08-25 16:33:34 +0800

    Integrate resource monitoring service

---
+++ src/renderer/services/ExecutorServices.tsx
@@ -0,0 +1,77 @@
+const url = process.env.TASK_EXECUTOR_URL;
+
+export async function stopExecutor() {
+  try {
+    const response = await fetch(`${url}/stop`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+    });
+    if (!response.ok) {
+      throw new Error(
+        `Failed to stop executor: ${response.statusText}`
+      );
+    }
+    const res = await response.json();
+    return res;
+  } catch (error) {
+    console.error('There was a problem with the fetch operation:', error);
+  }
+}
+
+export async function unpauseExecutor() {
+  try {
+    const response = await fetch(`${url}/unpause`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+    });
+    if (!response.ok) {
+      throw new Error(`Failed to unpause executor: ${response.statusText}`);
+    }
+    const res = await response.json();
+    return res;
+  } catch (error) {
+    console.error('There was a problem with the fetch operation:', error);
+  }
+}
+
+export async function pauseExecutor() {
+  try {
+    const response = await fetch(`${url}/pause`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+    });
+    if (!response.ok) {
+      throw new Error(`Failed to pause executor: ${response.statusText}`);
+    }
+    const res = await response.json();
+    return res;
+  } catch (error) {
+    console.error('There was a problem with the fetch operation:', error);
+  }
+}
+
+export async function getResourceStats() {
+  try {
+    const response = await fetch(`${url}/stats`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+    });
+    if (!response.ok) {
+      throw new Error(
+        `Failed to get resource statistics: ${response.statusText}`
+      );
+    }
+    const res = await response.json();
+    return res;
+  } catch (error) {
+    console.error('There was a problem with the fetch operation:', error);
+  }
+}
